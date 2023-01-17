56e0dfeef9178c47362bbfe572b3e4203b301301
maxim <zanoga.maxim@gmail.com>
2023-01-16 17:43:57 -0800

    add check by city or couyntry area

--- main.go
+++ main.go
@@ -9,20 +9,6 @@ import (
 )
 
 func main() {
-	db, err := geoip2.Open("geoip-data/GeoLite2-City.mmdb")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-	ip := net.ParseIP("4.10.15.22")
-	ipRecord, err := db.City(ip)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(ipRecord.Country.Names["en"])
-	fmt.Println(ipRecord.City.Names["en"])
-	fmt.Println(ipRecord.Postal.Code)
-	fmt.Println(ipRecord.Location.Latitude, ipRecord.Location.Longitude)
 
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
@@ -32,14 +18,48 @@ func main() {
 	})
 	r.GET("/ip", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"ip": "",
+			"ip": getClientIP(c),
 		})
 	})
-	err = r.Run(":8000")
+	r.GET("/geoip/:area/:ip", func(c *gin.Context) {
+		area := c.Param("area")
+		if area == "country" {
+			paramIP := c.Param("ip")
+			if paramIP != "" {
+				c.JSON(http.StatusOK, getGeoIPCountry(paramIP))
+			}
+		} else if area == "city" {
+			paramIP := c.Param("ip")
+			if paramIP != "" {
+				c.JSON(http.StatusOK, getGeoIPCity(paramIP))
+			}
+		} else {
+			c.JSON(http.StatusNotFound, JSONResponseError{"Invalid query"})
+		}
+	})
+	err := r.Run(":8000")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+}
+
+type JSONResponse struct {
+	IP         string  `json:"ip"`
+	Country    string  `json:"country"`
+	City       string  `json:"city"`
+	PostalCode string  `json:"postal_code"`
+	Latitude   float64 `json:"latitude"`
+	Longitude  float64 `json:"longitude"`
+}
+
+type JSONResponseCountry struct {
+	IP      string `json:"ip"`
+	Country string `json:"country"`
+}
+
+type JSONResponseError struct {
+	Error string `json:"error"`
 }
 
 func getClientIP(c *gin.Context) string {
@@ -51,4 +71,54 @@ func getClientIP(c *gin.Context) string {
 		ip = c.Request.RemoteAddr
 	}
 	return ip
+}
+
+func getGeoIPCity(ip string) JSONResponse {
+	db, err := geoip2.Open("geoip-data/GeoLite2-City.mmdb")
+	if err != nil {
+		panic(err)
+	}
+	defer func(db *geoip2.Reader) {
+		err := db.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(db)
+	parsedIP := net.ParseIP(ip)
+	ipRecord, err := db.City(parsedIP)
+	if err != nil {
+		panic(err)
+	}
+
+	return JSONResponse{
+		IP:         ip,
+		Country:    ipRecord.Country.Names["en"],
+		City:       ipRecord.City.Names["en"],
+		PostalCode: ipRecord.Postal.Code,
+		Latitude:   ipRecord.Location.Latitude,
+		Longitude:  ipRecord.Location.Longitude,
+	}
+}
+
+func getGeoIPCountry(ip string) JSONResponseCountry {
+	db, err := geoip2.Open("geoip-data/GeoLite2-Country.mmdb")
+	if err != nil {
+		panic(err)
+	}
+	defer func(db *geoip2.Reader) {
+		err := db.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(db)
+	parsedIP := net.ParseIP(ip)
+	ipRecord, err := db.Country(parsedIP)
+	if err != nil {
+		panic(err)
+	}
+
+	return JSONResponseCountry{
+		IP:      ip,
+		Country: ipRecord.Country.Names["en"],
+	}
 }
